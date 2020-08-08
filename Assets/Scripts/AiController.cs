c8d645dc4d78c6637469f87aa16ba3a926a0dac1
Schamberger12 <32084866+Schamberger12@users.noreply.github.com>
2020-08-08 02:59:43 -0500

    AI

---
+++ Assets/Scripts/AiController.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AiController : MonoBehaviour
+{
+
+    //Order of Precedence:
+    //1) Avoid obstacles
+    //2) Stay behind player
+    //Stay behind player as often as possible
+    //If an obstacle enters the AI's spherecollider,
+    //perform a raycast from the middle, tip of each wing
+    //top of fin, and bottom of wheels to check if will run into obstacle
+    //If it wont, continue to stay behind player
+
+    private bool isChecking = false;
+    private bool isAvoiding = false;
+
+    [SerializeField]
+    private Transform followPoint;
+
+    [SerializeField]
+    private GameObject[] rayArr = new GameObject[4];
+
+    LayerMask mask; 
+
+    // Ontriggerenter if obstacle enters sphereCollider, raycast to see if need
+    // to avoid, if need to avoid, do that!
+
+    // OnTriggerExit if player exits sphereCollider
+    // Move to follow player again
+
+    private void OnTriggerEnter(Collider other)
+    {
+        isChecking = true; 
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        isChecking = true; 
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        isChecking = false;
+    }
+
+    void Start()
+    {
+        mask = LayerMask.GetMask("Obstacles");
+    }
+
+
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float distance = Vector3.Distance(this.transform.position, followPoint.position); 
+        if (isAvoiding)
+        {
+            // Check if we're running into collider
+            
+            RaycastHit hit; 
+            foreach(GameObject point in rayArr)
+            {
+                if (Physics.Raycast(point.transform.position, point.transform.forward, out hit, 8f, mask))
+                {
+
+                }
+            }
+            
+        }
+        if (distance > )
+    }
+}
---
+++ Assets/Scripts/PlaneController.cs
@@ -0,0 +1,171 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaneController : MonoBehaviour
+{
+    [SerializeField]
+    private bool isPlayer = true; 
+
+    // Movement Variables
+    [SerializeField]
+    private float maxSpeed = 200f;
+    [SerializeField]
+    private float minSpeed = 0f; 
+    [SerializeField]
+    private float rotSpeed = 100f;
+    [SerializeField]
+    private float currSpeed = 0f; 
+    [SerializeField]
+    private float accel = 2.0f; 
+
+    private float roll = 0f;
+    private float pitch = 0f;
+    private float yaw = 0f;
+
+    private Rigidbody rb; 
+
+    private Quaternion plusRot = Quaternion.identity;
+
+    // To emulate value of Input.GetAxis(), lerping between current and next
+    private float currRoll = 0f; 
+    private float rollVal = 0f;
+    private float currPitch = 0f; 
+    private float pitchVal = 0f;
+    private float currYaw = 0f; 
+    private float yawVal = 0f;
+    private float thrustVal = 0f;
+    // Rolling over time
+    public float moveTime = 0.3f; 
+    public float rollTime = 0f;
+    public float pitchTime = 0f;
+    public float yawTime = 0f;
+
+    
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>(); 
+    }
+
+    // Input sends direction plane needs to move in
+    void MovePlane(string dir)
+    {
+        // Roll left is positive, right is negative
+        if (dir == "r_left")
+        {
+            rollVal = 1f;
+            rollTime += Time.fixedDeltaTime;
+            
+        }
+        else if (dir == "r_right")
+        {
+            rollVal = -1f; 
+            rollTime += Time.fixedDeltaTime;
+        }
+        else { rollVal = 0f; rollTime = 0f; }
+        // Pitch up is positive, down is negative
+        if (dir == "up")
+        {
+            pitchVal = 1f;
+            pitchTime += Time.fixedDeltaTime;
+        }
+        else if (dir == "down")
+        {
+            pitchVal = -1f;
+            pitchTime += Time.fixedDeltaTime;
+        }
+        else { pitchVal = 0f; pitchTime = 0f; }
+        // Yaw left is negative, right is positive
+        if (dir == "b_left")
+        {
+            yawVal = -1f;
+            yawTime += Time.fixedDeltaTime;
+        }
+        else if(dir == "b_right")
+        {
+            yawVal = 1f;
+            yawTime += Time.fixedDeltaTime;
+        }
+        else { yawVal = 0f; yawTime = 0f; }
+    }
+
+    void GetInput()
+    {
+        string dir = ""; 
+        if (Input.GetKey(KeyCode.A))
+        {
+            dir = "r_left";
+
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            dir = "r_right";
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            dir = "down";
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            dir = "up"; 
+        }
+        if (Input.GetKey(KeyCode.Q))
+        {
+            dir = "b_left";
+        }
+        if (Input.GetKey(KeyCode.E))
+        {
+            dir = "b_right";
+        }
+
+        MovePlane(dir); 
+
+       
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        GetInput();
+        //roll = Mathf.Lerp(currRoll, rollVal, rollTime / moveTime) * (Time.fixedDeltaTime*rotSpeed);
+        //pitch = Mathf.Lerp(currPitch, pitchVal, pitchTime / moveTime)* (Time.fixedDeltaTime * rotSpeed);
+        //yaw = Mathf.Lerp(currYaw, yawVal, yawTime / moveTime) * (Time.fixedDeltaTime * rotSpeed);
+
+        if (isPlayer)
+        {
+            roll = Input.GetAxis("Roll") * (Time.fixedDeltaTime * rotSpeed);
+            pitch = Input.GetAxis("Pitch") * (Time.fixedDeltaTime * rotSpeed);
+            yaw = Input.GetAxis("Yaw") * (Time.fixedDeltaTime * rotSpeed);
+        }
+        else
+        {
+            roll = rollVal * (Time.fixedDeltaTime * rotSpeed);
+            pitch = pitchVal * (Time.fixedDeltaTime * rotSpeed);
+            yaw = yawVal * (Time.fixedDeltaTime * rotSpeed);
+        }
+
+        currSpeed = Input.GetAxis("Accelerate") * (Time.fixedDeltaTime * accel);
+        if (currSpeed > maxSpeed)
+        {
+            currSpeed = maxSpeed;
+        }
+        if (currSpeed < minSpeed)
+        {
+            currSpeed = minSpeed; 
+        }
+        // Every frame, apply 
+        plusRot.eulerAngles = new Vector3(pitch, yaw, roll);
+        rb.rotation *= plusRot;
+        Vector3 plusPos = Vector3.forward;
+        plusPos = rb.rotation * plusPos;
+        rb.AddForce(plusPos * currSpeed);
+        //rb.velocity = plusPos * currSpeed; 
+        //rb.velocity = plusPos * (Time.fixedDeltaTime * currSpeed); 
+        
+    }
+}
